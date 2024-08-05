6716b44b9de308eca563611f5af77e06fe040825
janesalceda <33828700+janesalceda@users.noreply.github.com>
2024-08-05 11:16:01 +0800

    Create variables.tf

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,10 @@
+# modules/vpc/variables.tf
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block for the subnet"
+  type        = string
+}
