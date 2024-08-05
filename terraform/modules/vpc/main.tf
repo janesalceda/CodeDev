229dec55da5fd1e41097e6410f5edbf69e78e3cb
janesalceda <33828700+janesalceda@users.noreply.github.com>
2024-08-05 12:30:03 +0800

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -24,15 +24,3 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.main.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.web.id
-}
