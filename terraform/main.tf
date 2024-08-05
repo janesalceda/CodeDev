9c7ed9d327894d31e9d5cb92005337879d548b02
janesalceda <33828700+janesalceda@users.noreply.github.com>
2024-08-05 11:11:36 +0800

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "web" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI, replace with appropriate one
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+  security_groups = [aws_security_group.web.name]
+
+  root_block_device {
+    volume_size = 100
+  }
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "WebServer"
+  }
+}
