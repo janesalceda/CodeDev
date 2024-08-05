52f2256f79b7628508d2033383530d716b3039a4
janesalceda <33828700+janesalceda@users.noreply.github.com>
2024-08-05 12:28:34 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,18 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+provider "aws" {
+  region = var.region
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_cidr
+module "vpc" {
+  source     = "./modules/vpc"
+  vpc_cidr   = var.vpc_cidr
+  subnet_cidr = var.subnet_cidr
 }
 
-resource "aws_security_group" "web" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.main.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.web.id
+module "ec2_instance" {
+  source           = "./modules/ec2_instance"
+  ami              = var.ami
+  instance_type    = var.instance_type
+  subnet_id        = module.vpc.subnet_id
+  security_group_id = module.vpc.security_group_id
+  volume_size      = var.volume_size
 }
